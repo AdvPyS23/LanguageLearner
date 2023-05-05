c436eaf9ed783d50a0fe009a42c4afc19803550c
vespaquinn <quinncoxon@gmail.com>
2023-05-05 16:50:45 +0200

    Basics of UI integrated, creating a new language works, but not got an interface for existing languages yet. We need to work out how to make conditions that if any language button is pressed, it will open a generic 'language' menu, and set the current_language to that language. We need to find a way to do this that isn't just: if event == 'Spanish': current_lang = Spanish; if event == French: current_lang = French etc...

--- wordclass.py
+++ wordclass.py
@@ -1,5 +1,9 @@
+# Set Up (import modules)
 import pandas as pd
+from os import listdir, getcwd
+import PySimpleGUI as sg
 
+# Define Vocabulary class
 class Vocabulary:
     def __init__(self):
         self.data = pd.DataFrame(columns=['word', 'translation', 'pronunciation', 'example', 'category'])
@@ -21,14 +25,64 @@ class Vocabulary:
     def load_data(self, filename):
         self.data = pd.read_csv(filename)
 
-# Example usage
-vocabulary = Vocabulary()
-vocabulary.add_word('apple', 'pomme', 'æpl', 'I ate an apple for breakfast.', 'fruits')
-vocabulary.add_word('book', 'livre', 'bʊk', 'I like to read books in my free time.', 'education')
-vocabulary.add_word('cat', 'chat', 'kæt', 'My cat likes to play with string.', 'animals')
-vocabulary.save_data('vocabulary.csv')
-vocabulary.load_data('vocabulary.csv')
-vocabulary.get_words('fruits')
+# Example usage (test case)
+    # we dont want to run this every time so nest it in an if statement 
+if 1 == 0:
+    French = Vocabulary()
+    French.add_word('apple', 'pomme', 'æpl', 'I ate an apple for breakfast.', 'fruits')
+    French.add_word('book', 'livre', 'bʊk', 'I like to read books in my free time.', 'education')
+    French.add_word('cat', 'chat', 'kæt', 'My cat likes to play with string.', 'animals')
+    French.save_data('French.csv')
+    French.load_data('French.csv')
+    French.get_words('fruits')
 
-# Now This has an extra line 
 
+# Find languages already stored 
+path = getcwd()
+allfiles = listdir(path)
+languages = [filename for filename in allfiles if filename.endswith("csv")]
+languages = [filename.replace('.csv', '') for filename in languages]
+
+# Create different menus for the UI
+
+### Start a new language 
+def start_newlanguage():
+    layout = [ 
+        [sg.Text('Enter the name of the language',justification='center')],
+        [sg.Input(justification='center')],
+        [sg.Button('Enter'),sg.Button('Cancel')]]
+    window = sg.Window("Start a new language",layout)
+    while True:
+        event, values = window.read()
+        if event == "Cancel" or event == sg.WIN_CLOSED:
+            break
+        if event == "Enter":
+            languages.append(values[0])
+            globals()[values[0]] = Vocabulary()
+            break
+    window.close()
+    startup_window()
+
+            
+            
+
+def startup_window():
+
+    startup_layout = [  [sg.Text('Welcome! Select the language you want to practice, or start a new one :)')],
+                [sg.Button(lang) for lang in languages], 
+                [sg.Button('New Language...')],
+                [sg.Button('Quit')]]
+
+    startup_window = sg.Window('Window Title', startup_layout)
+    # Event Loop to process "events" and get the "values" of the inputs
+    while True:
+        event, values = startup_window.read()
+        if event == sg.WIN_CLOSED or event == 'Quit': # if user closes window or clicks Quit
+            break
+        if event == 'New Language...':
+            start_newlanguage()
+            break
+    startup_window.close()
+
+# Run the program
+startup_window()
